54bb7a98697f30abd288bee75aed3d702d729a07
EunKyung Shin <heybrilliante@gmail.com>
2021-10-14 23:43:39 +0900

    211014 실습1-2_여러값 중 최댓값

--- src/chap01/Max3m.java
+++ src/chap01/Max3m.java
@@ -2,10 +2,17 @@ package chap01;
 
 // 3개의 정숫값 가운데 최댓값을 구하여 출력합니다. 
 
-public class Max3m {
-
-	public static void main(String[] args) {
-
+class Max3m {
+	// a,b,c의 최댓값을 구하여 반환합니다. 
+	static int max3(int a, int b, int c) {
+		int max = a; // 최댓값
+		if(b > max)
+			max = b;
+		if(c > max)
+			max = c;
+		
+		return max; 
+		// 구한 최댓값을 호출한 곳으로 반환
 	}
 
 }
